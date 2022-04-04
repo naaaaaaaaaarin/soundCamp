2ce9b7eeea70739c494a211713bb9547a55b58ee
naaaaaaaaaarin <kal_1991@naver.com>
2022-04-04 10:06:21 +0900

    '22.04.04메인애니메이션추가'

---
+++ js/fadeIn.js
@@ -0,0 +1,13 @@
+const saTriggerMargin = 300;
+const saElementList = document.querySelectorAll('.sa');
+
+const saFnc = function () {
+  for (const element of saElementList) {
+    if (!element.classList.contains('show')) {
+      if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+        element.classList.add('show');
+      };
+    };
+  };
+};
+window.addEventListener('scroll', saFnc);
